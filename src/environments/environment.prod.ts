b4910bcda70eb6b76d12623c9ccabb64b2bad58a
yaairnaavaa <iryanavahe@ittepic.edu.mx>
2019-08-16 11:59:02 -0600

    Add configFirebase to  production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDBJQc2NAZuPUahVGZdwfxm8BvOtaYj_gI",
+    authDomain: "ceremonia-graduacion.firebaseapp.com",
+    databaseURL: "https://ceremonia-graduacion.firebaseio.com",
+    projectId: "ceremonia-graduacion",
+    storageBucket: "ceremonia-graduacion.appspot.com",
+    messagingSenderId: "452486540507",
+    appId: "1:452486540507:web:43e9972ade946c9d"
+  }
 };
