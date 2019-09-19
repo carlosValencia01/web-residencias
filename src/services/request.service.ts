98ffaed934ecc04dfd2298fbd24f9d4cdb753d99
Agustin Barajas <agbarajasva@ittepic.edu.mx>
2019-09-18 21:57:31 -0600

    fix: Generation of pdf files
    Change the font to Montserrat in tables and change the logo sizes in the header

--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { iRequest } from 'src/entities/request.model';
 import { eRequest } from 'src/enumerators/request.enum';
+
 @Injectable()
-export class RequestService{
-    private Request:iRequest;     
-    public requestUpdate=new EventEmitter<{Request:iRequest, Phase:eRequest}>();
-    
-    public AddRequest(request:iRequest, phase:eRequest):void{        
-        this.Request=request;
-        console.log("ADD REQUEST service", this.Request);
+export class RequestService {
+    private Request: iRequest;
+    public requestUpdate = new EventEmitter<{Request: iRequest, Phase: eRequest}>();
+
+    public AddRequest(request: iRequest, phase: eRequest): void {
+        this.Request = request;
+        console.log('ADD REQUEST service', this.Request);
         this.requestUpdate.emit({Request: this.Request, Phase: phase});
-    }    
+    }
 }
