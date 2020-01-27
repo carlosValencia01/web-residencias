037f50b85335bdaf8f3425c73623efb5404672d6
Agustin Barajas <agbarajasva@ittepic.edu.mx>
2020-01-27 00:15:54 -0700

    fix: Add employee email

--- src/pages/reception-act/grade-page/grade-page.component.ts
+++ src/pages/reception-act/grade-page/grade-page.component.ts
@@ -2,8 +2,6 @@ import {Component, Injectable, OnInit, ViewChild} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {ActivatedRoute, Router} from '@angular/router';
-import * as Papa from 'papaparse';
-import Swal from 'sweetalert2';
 
 import {CookiesService} from 'src/services/app/cookie.service';
 import {EmployeeGradeComponent} from 'src/modals/reception-act/employee-grade/employee-grade.component';
@@ -11,7 +9,6 @@ import {EmployeeProvider} from 'src/providers/shared/employee.prov';
 import {eNotificationType} from 'src/enumerators/app/notificationType.enum';
 import {eOperation} from 'src/enumerators/reception-act/operation.enum';
 import {IEmployee} from 'src/entities/shared/employee.model';
-import {LoadCsvDataComponent} from 'src/modals/shared/load-csv-data/load-csv-data.component';
 import {NotificationsServices} from 'src/services/app/notifications.service';
 import {UploadEmployeesCsvComponent} from 'src/modals/electronic-signature/upload-employees-csv/upload-employees-csv.component';
 
@@ -42,7 +39,7 @@ export class GradePageComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.displayedColumns = ['fullName', 'rfc', 'grades', 'positions', 'action'];
+    this.displayedColumns = ['fullName', 'rfc', 'email', 'grades', 'positions', 'action'];
     this.getEmployees();
   }
 
@@ -60,6 +57,7 @@ export class GradePageComponent implements OnInit {
     const tmp: IGradeTable = {
       _id: employee._id,
       rfc: employee.rfc,
+      email: employee.email,
       fullName: employee.name.fullName,
       grades: employee.grade ? employee.grade.length : 0,
       positions: employee.positions ? employee.positions.filter(item => item.status === 'ACTIVE').length : 0,
@@ -139,76 +137,6 @@ export class GradePageComponent implements OnInit {
     });
   }
 
-  // public onUpload(event) {
-  //   const notification = this.notificationServ;
-  //   const provider = this.employeeProvider;
-  //   const arrayData: any[] = [];
-  //   if (event.target.files && event.target.files[0]) {
-  //     Papa.parse(event.target.files[0], {
-  //       complete: async results => {
-  //         if (results.data.length > 0) {
-  //           const elements = results.data;
-  //           await this._asyncForEach(elements, async (element, index) => {
-  //             if (index > 0) {
-  //               const employeeIndex = arrayData.findIndex(x => x.rfc === element[0]);
-  //               if (employeeIndex !== -1) {
-  //                 const {value: updateEmployee} = await Swal.fire({
-  //                   title: 'Empleado duplicado',
-  //                   text: `El empleado con RFC ${element[0]}, está duplicado. ¿Desea actualizar o descartar los nuevos datos?`,
-  //                   type: 'question',
-  //                   allowOutsideClick: false,
-  //                   showCancelButton: true,
-  //                   confirmButtonColor: 'blue',
-  //                   cancelButtonColor: 'red',
-  //                   confirmButtonText: 'Actualizar',
-  //                   cancelButtonText: 'Descartar',
-  //                   focusConfirm: true
-  //                 });
-  //                 if (updateEmployee) {
-  //                   arrayData.splice(employeeIndex, 1);
-  //                   arrayData.push(this._buildPreviousStructure(element));
-  //                 }
-  //               } else {
-  //                 if (elements[index].length >= 6) {
-  //                   arrayData.push(this._buildPreviousStructure(element));
-  //                 }
-  //               }
-  //             }
-  //           });
-  //           const _displayedColumns = ['rfc', 'curp', 'firstName', 'lastName', 'gender', 'birthDate'];
-  //           const _displayedColumnsName = ['RFC', 'CURP', 'Nombres', 'Apellidos', 'Género', 'Fecha de nacimiento'];
-  //           const dialogRef = this.dialog.open(LoadCsvDataComponent, {
-  //             id: 'LoadEmployeeCsvData',
-  //             data: {
-  //               config: {
-  //                 title: 'Empleados cargados',
-  //                 displayedColumns: _displayedColumns,
-  //                 displayedColumnsName: _displayedColumnsName
-  //               },
-  //               componentData: arrayData
-  //             },
-  //             disableClose: true,
-  //             hasBackdrop: true,
-  //             width: '65em',
-  //           });
-  //           dialogRef.afterClosed()
-  //             .subscribe(_employees => {
-  //               if (_employees && _employees.length) {
-  //                 const _arrayEmployees = _employees.map(this._buildEmployeeStructure);
-  //                 provider.csvEmployeGrade(_arrayEmployees).subscribe(_ => {
-  //                   notification.showNotification(eNotificationType.SUCCESS, 'Los empleados se han guardado con éxito', '');
-  //                   this.refreshEmployees();
-  //                 }, _ => {
-  //                   notification.showNotification(eNotificationType.ERROR, 'Ha ocurrido un error al importar los empleados', '');
-  //                 });
-  //               }
-  //             });
-  //         }
-  //       }
-  //     });
-  //   }
-  // }
-
   public applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
     if (this.dataSource.paginator) {
@@ -245,6 +173,7 @@ export class GradePageComponent implements OnInit {
 interface IGradeTable {
   _id?: string;
   rfc?: string;
+  email?: string;
   fullName?: string;
   grades?: number;
   positions?: number;
--- src/providers/app/api.prov.ts
+++ src/providers/app/api.prov.ts
@@ -4,10 +4,9 @@ import { CookiesService } from 'src/services/app/cookie.service';
 
 @Injectable()
 export class Api {
-    // url = 'http://localhost:3004/escolares/credenciales';
-    url = 'http://localhost:3003/escolares/credenciales';
-    // url = 'https://api.cideti.com.mx/escolares/credenciales';
-    // url = 'http://104.248.94.77/escolares/credenciales';
+    // url = 'http://localhost:3003/escolares/credenciales';
+    url = 'https://api.cideti.com.mx/escolares/credenciales';
+    // url = 'https://rijimenezesdev.me/escolares/credenciales';
     urlE = 'http://localhost:3000/escolares/credenciales';
 
     headers: Headers = new Headers();
