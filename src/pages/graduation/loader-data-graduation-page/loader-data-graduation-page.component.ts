54ed11a3f07d7afaf57a2bb6f65a64b907dd0410
rijimenezes <rijimenezes@ittepic.edu.mx>
2020-02-01 20:32:54 -0700

    Feat: load graduates

--- src/pages/graduation/loader-data-graduation-page/loader-data-graduation-page.component.ts
+++ src/pages/graduation/loader-data-graduation-page/loader-data-graduation-page.component.ts
@@ -24,7 +24,7 @@ export class LoaderDataGraduationPageComponent implements OnInit {
   pageSize = 10;
   students = [];
   loading : boolean = false;
-
+  displayStudents = [];
   careers = {
     'INGENIERÍA BIOQUÍMICA': 'IBQ',
     'INGENIERÍA EN GESTIÓN EMPRESARIAL': 'IGE',
@@ -118,10 +118,12 @@ export class LoaderDataGraduationPageComponent implements OnInit {
             nombreApellidos: tmpStudent[2],
             nombre: tmpStudent[3],
             carreraCompleta: tmpStudent[4],
-            carrera: this.careers[tmpStudent[4].trim()],
+            carrera: this.careers[tmpStudent[4]] ? this.careers[tmpStudent[4].trim()] : '',
             especialidad: tmpStudent[5].replace('%20',','),
             promedio: tmpStudent[6],
             correo: tmpStudent[7],
+            genero: tmpStudent[8],
+            curp: tmpStudent[9],
             observations: '',
             degree: '',
             estatus: 'Registrado',
@@ -146,53 +148,87 @@ export class LoaderDataGraduationPageComponent implements OnInit {
       // }
     });
     // console.log(this.csvObjects);
-    
+    const sub = this.firebaseService.getGraduates(this.collection).subscribe(
+      (students)=>{
+        sub.unsubscribe();
+        const sts = students.map( (st)=> ({nc:st.payload.doc.get('nc'),id:st.payload.doc.id,genero:st.payload.doc.get('genero')}));                    
+                             
+          if(sts.length>0){                         
+            for (const student of this.csvObjects){ 
+              const stu = sts.filter( st=> st.nc == student.nc)[0];             
+              if(!stu){
+                this.displayStudents.push(student);
+              }
+            }  
+          }else{
+            this.displayStudents = this.csvObjects;
+          }
+      }
+    );
   }
 
   async sendData() {
+    let i=0;
     this.loading=true;
     if (this.type === '0') {
-
-      // let students = ;
-      for await (const student of this.csvObjects){
-        await this.firebaseService.loadCSV(student, this.collection).then(resp => {
-        }).catch(err => {});
-        await this.firebaseService.asignEvent(this.collection
-          ,student.nc).then((col=>{
-          })).catch(err=>{});            
-      }  
-      const sst =this.firebaseService.getGraduates(this.collection).subscribe(
-        async (stds)=>{
-          sst.unsubscribe();
-          const students = stds.map( (std)=> ({id:std.payload.doc.id,promedio:std.payload.doc.get('promedio'), carrera:std.payload.doc.get('carrera')}));
-          const sb = this.firebaseService.getCareers().subscribe(
-            async (careers :any)=>{
-              sb.unsubscribe();                    
-              for await (const career of careers){
-            
-                const studentsCareer = students.filter( (st)=> st.carrera+'' == career.nombre+'').map( std=> std.promedio);
-                const greaterAvg = studentsCareer.length > 0 ? Math.max(...studentsCareer) : 0;
-                
-                const stGreaterAvg = greaterAvg > 0 ? students.filter( (st)=> st.carrera+'' == career.nombre+'').filter( (st)=> st.promedio == greaterAvg) : false;
-                if(stGreaterAvg){
-                  
-                  for await (const bestAvg of stGreaterAvg){
-                    console.log(bestAvg);
-                    await this.firebaseService.updateFieldGraduate(bestAvg.id,{mejorPromedio:true},this.collection).then( up=>{}).catch(err=>{});
-                  }
+      let objects = this.csvObjects;      
+      const sub = this.firebaseService.getGraduates(this.collection).subscribe(
+        async (students)=>{
+          const sts = students.map( (st)=> ({nc:st.payload.doc.get('nc'),id:st.payload.doc.id,genero:st.payload.doc.get('genero')}));                    
+                             
+          sub.unsubscribe();
+          
+          if(sts.length>0){            
+                        
+            for await (const student of objects){ 
+              const stu = sts.filter( st=> st.nc == student.nc)[0];             
+              if(!stu){
+                await this.firebaseService.loadCSV(student, this.collection).then(resp => {
+                }).catch(err => {});
+                await this.firebaseService.asignEvent(this.collection
+                  ,student.nc).then((col=>{
+                  })).catch(err=>{});            
+              }else{
+                if(!stu.genero){
+                  await this.firebaseService.updateFieldGraduate(stu.id,{genero:student.genero,curp:student.curp},this.collection).then(up=>{});
                 }
-                
-                
-                
               }
+            }  
+          }else{
+            for await (const student of objects){
+              await this.firebaseService.loadCSV(student, this.collection).then(resp => {
+              }).catch(err => {});
+              await this.firebaseService.asignEvent(this.collection
+                ,student.nc).then((col=>{
+                })).catch(err=>{});            
               
-            }
-          );
+            }  
+          }
+          
         }
       );
+      if(this.displayStudents.length>=100){
+        setTimeout(() => {
+          setTimeout(() => {
+            
+            this.updateBestAvg();
+          }, 60000);
+          this.notificationsServices.showNotification(0, 'Exito',' Alumnos registrados');
+          this.loading=false;
+          this.cancel();
+        }, 30000);
+      }else{
+        setTimeout(() => {
+            
+          this.updateBestAvg();
+        }, 60000);
+        this.notificationsServices.showNotification(0, 'Exito',' Alumnos registrados');
+        this.loading=false;
+        this.cancel();
+      }
+
       
       
-      this.notificationsServices.showNotification(0, 'Exito', 'Alumnos registrados correctamente');
     }
     if (this.type === '1') {
       this.csvObjects.forEach( student => {
@@ -203,10 +239,55 @@ export class LoaderDataGraduationPageComponent implements OnInit {
         );
       });
     }
-    this.loading=false;
+    
     this.cancel();
   }
 
+  updateBestAvg(){
+    const sst =this.firebaseService.getGraduates(this.collection).subscribe(
+      async (stds)=>{
+        sst.unsubscribe();
+               
+          const students = stds.map( (std)=> ({id:std.payload.doc.id,promedio:std.payload.doc.get('promedio'), carrera:std.payload.doc.get('carrera'),mejorPromedio:std.payload.doc.get('mejorPromedio')}));
+          const sb = this.firebaseService.getCareers().subscribe(
+            async (careers :any)=>{
+              sb.unsubscribe();                    
+              for await (const career of careers){
+                const prevBestAvg = students.filter(st=> st.mejorPromedio == true && st.carrera+'' == career.nombre+'')[0];
+                const studentsCareer = students.filter( (st)=> st.carrera+'' == career.nombre+'').map( std=> std.promedio);
+                const greaterAvg = studentsCareer.length > 0 ? Math.max(...studentsCareer) : false;              
+                
+                const stGreaterAvg = greaterAvg > 0 ? students.filter( (st)=> st.carrera+'' == career.nombre+'').filter( (st)=> st.promedio == greaterAvg) : false;           
+                
+                if(stGreaterAvg){
+                  if(prevBestAvg){
+
+                    const isBestAvg = stGreaterAvg.filter( st=> st.id == prevBestAvg.id).length === 1;
+                    if(!isBestAvg){
+                      const best = stGreaterAvg.filter( st=> st.promedio >= prevBestAvg.promedio);
+                      if(best.length>0){
+                        await this.firebaseService.updateFieldGraduate(prevBestAvg.id,{mejorPromedio:false},this.collection).then(up=>{});
+                        for await (const bestAvg of best){                            
+                          await this.firebaseService.updateFieldGraduate(bestAvg.id,{mejorPromedio:true},this.collection).then( up=>{}).catch(err=>{});
+                        }
+                      }
+                    }
+                  }else{
+                    for await (const bestAvg of stGreaterAvg){                      
+                      await this.firebaseService.updateFieldGraduate(bestAvg.id,{mejorPromedio:true},this.collection).then( up=>{}).catch(err=>{});
+                    }
+                  }
+                }
+                
+                
+                
+              }
+              
+            }
+          );
+        }                    
+    );
+  }
   sendOneMail(item) {
       this.graduationProv.sendQR(item.correo, item.id, item.nombre).subscribe(
         res => {
@@ -221,6 +302,7 @@ export class LoaderDataGraduationPageComponent implements OnInit {
   cancel() {
     this.fileName = 'Seleccione un archivo';
     this.csvObjects = [];
+    this.displayStudents = [];  
   }
 
   // para cambiar el contador de las paginas
