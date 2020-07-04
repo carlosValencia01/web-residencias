3920bb66b132d8d54631091ad80e27d1510191d1
Agustin Barajas <agbarajasva@ittepic.edu.mx>
2020-07-03 18:47:57 -0600

    feat: Change signature password and download file

--- src/app/rrhh/electronic-signature/electronic-signature.component.ts
+++ src/app/rrhh/electronic-signature/electronic-signature.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {eNotificationType} from 'src/app/enumerators/app/notificationType.enum';
-import {EmployeeProvider} from 'src/app/providers/shared/employee.prov';
-import {ESignatureProvider} from 'src/app/providers/electronic-signature/eSignature.prov';
-import {CookiesService} from 'src/app/services/app/cookie.service';
-import {CurrentPositionService} from 'src/app/services/shared/current-position.service';
-import {NotificationsServices} from 'src/app/services/app/notifications.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { eNotificationType } from 'src/app/enumerators/app/notificationType.enum';
+import { EmployeeProvider } from 'src/app/providers/shared/employee.prov';
+import { ESignatureProvider } from 'src/app/providers/electronic-signature/eSignature.prov';
+import { CookiesService } from 'src/app/services/app/cookie.service';
+import { CurrentPositionService } from 'src/app/services/shared/current-position.service';
+import { NotificationsServices } from 'src/app/services/app/notifications.service';
 import Swal from 'sweetalert2';
 import * as moment from 'moment';
 moment.locale('es');
@@ -31,32 +31,32 @@ export class ElectronicSignatureComponent implements OnInit {
 
   constructor(
     private employeeProvider: EmployeeProvider,
-    private eSignatureProvider: ESignatureProvider ,
+    private eSignatureProvider: ESignatureProvider,
     private cookiesService: CookiesService,
     private currentPositionService: CurrentPositionService,
     private notification: NotificationsServices) {
-      this.formGroupPsw = new FormGroup({
-        'psw': new FormControl(null, [
-          Validators.required
-        ]),
-        'confPsw': new FormControl(null, [
-          Validators.required
-        ]),
-        'loginPsw': new FormControl(null, [
-          Validators.required
-        ])
-      });
-      this.formGroupPswChange = new FormGroup({
-        'pswChange': new FormControl(null, [
-          Validators.required
-        ]),
-        'confPswChange': new FormControl(null, [
-          Validators.required
-        ]),
-        'eSigPswChange': new FormControl(null, [
-          Validators.required
-        ])
-      });
+    this.formGroupPsw = new FormGroup({
+      'psw': new FormControl(null, [
+        Validators.required
+      ]),
+      'confPsw': new FormControl(null, [
+        Validators.required
+      ]),
+      'loginPsw': new FormControl(null, [
+        Validators.required
+      ])
+    });
+    this.formGroupPswChange = new FormGroup({
+      'pswChange': new FormControl(null, [
+        Validators.required
+      ]),
+      'confPswChange': new FormControl(null, [
+        Validators.required
+      ]),
+      'eSigPswChange': new FormControl(null, [
+        Validators.required
+      ])
+    });
   }
 
   ngOnInit() {
@@ -68,18 +68,18 @@ export class ElectronicSignatureComponent implements OnInit {
 
     this.user = this.cookiesService.getData().user;
 
-    this.employeeProvider.getEmployee(this.user.email).subscribe( res => {
+    this.employeeProvider.getEmployee(this.user.email).subscribe(res => {
       this.employee = res;
       this.fileName = res.employee.rfc + this.currentPosition.name;
 
       this.eSignatureProvider.hasESignature(res.employee.rfc, this.currentPosition._id)
-        .subscribe( data => {
+        .subscribe(data => {
           const expireDate = moment(new Date(data.expireDate).setHours(0, 0, 0, 0));
           const today = new Date().setHours(0, 0, 0, 0);
           const twoWeeksAfter = moment(today).add(2, 'weeks');
           this.expiredSignature = expireDate.isSameOrBefore(today);
-          this.expiredMessage =  this.expiredSignature ? 'Su firma electrónica ha expirado' : 'Su firma electrónica está por expirar';
-  
+          this.expiredMessage = this.expiredSignature ? 'Su firma electrónica ha expirado' : 'Su firma electrónica está por expirar';
+
           this.canUpdateSignature = expireDate.isBetween(today, twoWeeksAfter) || expireDate.isSameOrBefore(today)
           this.eSignatureStatus = moment(data.expireDate).format('LL');
           if (this.canUpdateSignature) {
@@ -127,7 +127,7 @@ export class ElectronicSignatureComponent implements OnInit {
       .subscribe(data => {
         this.canUpdateSignature = false;
         this.expiredMessage = '';
-        this.expiredSignature =  false;
+        this.expiredSignature = false;
         this.formGroupPsw.reset();
         this.formGroupPsw.disable();
         const currentDate = new Date();
@@ -137,28 +137,14 @@ export class ElectronicSignatureComponent implements OnInit {
         const expireDate = new Date(year, month, day);
         this.eSignatureStatus = moment(expireDate).format('LL');
         this.notification.showNotification(eNotificationType.SUCCESS, data.message, '');
-    }, error => {
-      this.notification.showNotification(eNotificationType.ERROR, error.message, '');
-    });
+      }, error => {
+        this.notification.showNotification(eNotificationType.ERROR, error.message, '');
+      });
   }
 
   getDocument() {
     this.eSignatureProvider.getDocument(this.employee.employee._id, this.currentPosition._id).subscribe(data => {
-      if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveOrOpenBlob(data.text(), `${this.fileName}.itt`);
-      } else {
-        const newBlob = new Blob([data.text()], {type: 'text/plain'});
-        const url = URL.createObjectURL(newBlob);
-        const a = document.createElement('a');
-
-        document.body.appendChild(a);
-        a.setAttribute('style', 'display: none');
-        a.href = url;
-        a.download = `${this.fileName}.itt`;
-        a.click();
-        window.URL.revokeObjectURL(url);
-        a.remove();
-      }
+      this._downloadSignatureFile(data.text());
     });
   }
 
@@ -167,28 +153,49 @@ export class ElectronicSignatureComponent implements OnInit {
       this.eSignatureProvider.changeESignaturePassword(this.user._id, this.employee.employee._id, this.currentPosition._id, {
         eSigPsw: this.formGroupPswChange.get('eSigPswChange').value,
         newPsw: this.formGroupPswChange.get('pswChange').value,
-      }).subscribe(res => {
-          if (res.status) {
-            this.notification.showNotification(eNotificationType.ERROR, 'Firma electrónica', res.message);
-          } else {
-            this.notification.showNotification(eNotificationType.SUCCESS, 'Firma electrónica', 'Contraseña actualizada correctamente');
-            this.formGroupPswChange.reset();
-          }
-        }, err => {
-          const err_msj = JSON.parse(err && err._body || '').err || '';
-          this.notification.showNotification(eNotificationType.ERROR, 'Firma electrónica', err_msj || 'No se pudo cambiar la contraseña');
+      }).subscribe((res: { doc: string, message: string }) => {
+        this.formGroupPswChange.reset();
+        this.notification
+          .showNotification(eNotificationType.SUCCESS, 'Firma electrónica', res.message || 'Contraseña actualizada correctamente');
+        if (res.doc) {
+          this._downloadSignatureFile(res.doc);
+        } else {
+          this.notification
+            .showNotification(eNotificationType.INFORMATION, 'Firma electrónica', 'Debe cambiar su documento de firma, para ello solo debe descargarlo nuevamente');
         }
-      );
+      }, (err: { _body: string }) => {
+        const err_msj = JSON.parse(err && err._body || '').message || '';
+        this.notification.showNotification(eNotificationType.ERROR, 'Firma electrónica', err_msj || 'No se pudo cambiar la contraseña');
+      });
     } else {
       this.notification.showNotification(eNotificationType.ERROR, 'Firma electrónica', 'Las contraseñas no coinciden');
     }
   }
 
   validateMatchPassword() {
-    this.isMatchPasswords = this.formGroupPsw.get('psw').value === this.formGroupPsw.get('confPsw').value ;
+    this.isMatchPasswords = this.formGroupPsw.get('psw').value === this.formGroupPsw.get('confPsw').value;
   }
 
   validateMatchPasswordChange() {
-    this.isMatchPasswordsChange = this.formGroupPswChange.get('pswChange').value === this.formGroupPswChange.get('confPswChange').value ;
+    this.isMatchPasswordsChange = this.formGroupPswChange.get('pswChange').value === this.formGroupPswChange.get('confPswChange').value;
   }
+
+  private _downloadSignatureFile(data: string): void {
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(data, `${this.fileName}.itt`);
+    } else {
+      const newBlob = new Blob([data], { type: 'text/plain' });
+      const url = URL.createObjectURL(newBlob);
+      const a = document.createElement('a');
+
+      document.body.appendChild(a);
+      a.setAttribute('style', 'display: none');
+      a.href = url;
+      a.download = `${this.fileName}.itt`;
+      a.click();
+      window.URL.revokeObjectURL(url);
+      a.remove();
+    }
+  }
+
 }
